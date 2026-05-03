39cc115a477eadb06a9bbdfa490f55fbd532b831
LAKSHYA CHOUDHARY <choudharylakshya.lc.com>
2026-05-04 00:24:47 +0530

    resetPassword page setup

--- server/src/services/mail.js
+++ server/src/services/mail.js
@@ -1,47 +1,52 @@
 import nodemailer from "nodemailer";
-import { NODEMAILER_FROM_EMAIL, NODEMAILER_PASS } from "../config/config.js";
-import {emailVerificationTemplate ,sendforgotPasswordLinkEmailTemplate } from "./mail/templete.js"
+import {
+  NODEMAILER_FROM_EMAIL,
+  NODEMAILER_PASS
+} from "../config/config.js";
+import {
+  emailVerificationTemplate,
+  sendforgotPasswordLinkEmailTemplate
+} from "./mail/templete.js";
 
+// Create transporter once globally
 const transporter = nodemailer.createTransport({
-     service:"gmail",
-     auth:{
-          user:NODEMAILER_FROM_EMAIL,
-          pass:NODEMAILER_PASS
-     }
-})
+  
+  service:"gmail",
 
-export const sendEmailVerification = async (to,otp) => {
-     try {
-          const res = await transporter.sendMail({
-               from:NODEMAILER_FROM_EMAIL,
-               to,
-               subject: "Email Verification",
-               html: emailVerificationTemplate(otp)
-          })
+  auth: {
+    user: NODEMAILER_FROM_EMAIL,
+    pass: NODEMAILER_PASS
+  },
 
-          console.log("Email sent successfully",res);
-     }
-     catch(err){
-          console.log("Error sending mail",err);
-     }
-}
-
-export const sendforgotPasswordLinkEmail = async (to,link) => {
-     try {
-          const res = await transporter.sendMail({
-               from:NODEMAILER_FROM_EMAIL,
-               to,
-               subject: "Password Reset",
-               html: sendforgotPasswordLinkEmailTemplate(link)
-          })
-
-          console.log("Email sent successfully",res);
-     }
-     catch(err){
-          console.log("Error sending mail",err);
-     }
-}
-          
+});
 
 
+export const sendEmailVerification = async (to, otp) => {
+  try {
+    const res = await transporter.sendMail({
+      from: NODEMAILER_FROM_EMAIL,
+      to,
+      subject: "Email Verification",
+      html: emailVerificationTemplate(otp)
+    });
 
+    console.log("Verification Email Sent:", res.response);
+  } catch (err) {
+    console.log("Error sending verification email:", err);
+  }
+};
+
+export const sendforgotPasswordLinkEmail = async (to, link) => {
+  try {
+    const res = await transporter.sendMail({
+      from: NODEMAILER_FROM_EMAIL,
+      to,
+      subject: "Password Reset",
+      html: sendforgotPasswordLinkEmailTemplate(link)
+    });
+
+    console.log("Reset Email Sent:", res.response);
+  } catch (err) {
+    console.log("Error sending reset email:", err);
+  }
+};
